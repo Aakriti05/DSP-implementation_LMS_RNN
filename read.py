b53aba94ada93dd16aaae6701870875ab4bc9759
Aakriti05 <juhi05aakritiagrawal@gmail.com>
2018-11-30 23:10:53 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,23 @@
+import h5py
+
+def traverse_datasets(hdf_file):
+
+    def h5py_dataset_iterator(g, prefix=''):
+        for key in g.keys():
+            item = g[key]
+            path = '{0}/{1}'.format(prefix, key)
+            if isinstance(item, h5py.Dataset): # test for dataset
+                yield (path, item)
+            elif isinstance(item, h5py.Group): # test for group (go down)
+                yield from h5py_dataset_iterator(item, path)
+
+    with h5py.File(hdf_file, 'r') as f:
+        for path, _ in h5py_dataset_iterator(f):
+            yield path
+
+
+with h5py.File('rnnweights.h5', 'r') as f:
+    for dset in traverse_datasets(f):
+        print('Path:', dset)
+        print('Shape:', f[dset].shape)
+        print('Data type:', f[dset].dtype)
